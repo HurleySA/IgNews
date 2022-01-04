b975685d018511718c463657831cb159b083ac22
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-01-04 15:58:47 -0300

    Iniciando Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Head from "next/head";
 export default function Home() {
     return (
-     <h1>
+      <>
        <Head>
          <title>Inicio - Ignews</title>
        </Head>
-       Hello <span>World</span> 
-       </h1>
+      </>
     )
   }
   
