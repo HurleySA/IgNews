976badcf8d74ae585cdcda67647710777c8ac594
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-01-04 14:57:52 -0300

    Adicionando estilos globais

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+export default function Home() {
+    return (
+     <h1>
+       <Head>
+         <title>Inicio - Ignews</title>
+       </Head>
+       Hello <span>World</span> 
+       </h1>
+    )
+  }
+  
