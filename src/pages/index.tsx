551f21c5816fa0427f0cb947cf468b2712eae77a
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-01-04 17:34:26 -0300

    Adicionando Subscribe Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss"
 export default function Home() {
     return (
@@ -15,6 +16,7 @@ export default function Home() {
              Get acess to all the publications <br/>
              <span>For $9.90 month</span>
            </p>
+           <SubscribeButton/>
          </section>
 
          <img src="/images/avatar.svg" alt="" />
