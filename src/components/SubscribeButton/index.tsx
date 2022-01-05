97e71260fb012bf2d31febd60cd2ed1e0ebd261e
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-01-04 22:14:48 -0300

    Adicionando conexão ao Stripe e SSR

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,8 @@
 import styles from "./styles.module.scss"
-export function SubscribeButton(){
+interface SubscribeButtonProps{
+    priceId: String,
+}
+export function SubscribeButton({priceId} : SubscribeButtonProps){
     return(
         <button type="button" className={styles.subscribeButton}>Subscribe Now</button>
     )
