186923c6fca3ed68d6a9d74a62a70146d7a6697d
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-01-05 08:11:27 -0300

    Transformando em SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -34,7 +34,7 @@ export default function Home({product}: HomeProps) {
     )
   }
 
-  export const getServerSideProps: GetServerSideProps = async () =>{
+  export const getStaticProps: GetStaticProps = async () =>{
     const price = await stripe.prices.retrieve("price_1JjkK1AERVbKXB5ZYbzNJwJ5", {
       expand:['product']
     })
@@ -50,7 +50,8 @@ export default function Home({product}: HomeProps) {
     return {
       props:{
         product
-      }
+      },
+      revalidate: 60 * 60 *24,
 
     }
   }
