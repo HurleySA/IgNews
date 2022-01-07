84ffb9f1908cd5369e50e18b55c1eace2200fd89
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-01-06 23:19:51 -0300

    Adicionando integração com Banco de Dados e adicionando usuários não repetido ao BD

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { Create, Collection, If, Not, Exists, Match, Index, Casefold, Get } from "faunadb";
+
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
     providers: [
@@ -9,7 +12,33 @@ export default NextAuth({
           authorization: { params: {  scope: 'read:user'}},
         }),
 
-    ]
+    ],
+    secret: process.env.SECRET_KEY
+    ,
+    callbacks:{
+      async signIn({user, account, profile}){   
+        const { email } = user;
+
+        try{
+          await fauna.query(
+            If(Not(Exists(Match(Index('user_by_email'), Casefold(user.email)))),
+            Create(
+              Collection('users'),
+              {
+                data: { email }
+              }
+            ),
+            Get(Match(Index('user_by_email'), Casefold(user.email)))
+            )
+           
+          )
+          return true;
+        }catch {
+          return false;
+        }
+       
+      },
+    }
 
 
 })
